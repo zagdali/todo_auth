0ace78fffbfdb8449ac7e910a1475d2280ef3537
Мартин Анкру <votb02511@yandex.ru>
2026-01-29 23:01:58 +0300

    [+] создание архитектуры проекта R: добавление в ветку архитектуры проекта FB: багов пока нет

---
+++ app/config/settings.py
@@ -0,0 +1,8 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+   pass
+
+
+settings = Settings()
---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from .routers import users, projects, lists, tasks, tags
+from .database import engine
+from .models import Base
+
+app = FastAPI(title="TODOLIST")
+
+app.include_router(users.router, prefix="/users", tags=["users"])
+app.include_router(projects.router, prefix="/projects", tags=["projects"])
+app.include_router(lists.router, prefix="/lists", tags=["lists"])
+app.include_router(tasks.router, prefix="/tasks", tags=["tasks"])
+app.include_router(tags.router, prefix="/tags", tags=["tags"])
